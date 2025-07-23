31a5c41a8d8b2b96331658d24372dd11ac1e83f1
Umang <umangaggarwal7599@gmail.com>
2025-07-24 03:05:32 +0530

    some bugs fixed

--- app/save/insights.tsx
+++ app/save/insights.tsx
@@ -1,10 +1,41 @@
 // app/save/option1.tsx
-import { View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Option1() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Option 1 Page</Text>
+   <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.push("/(tabs)/save")}>
+          <Ionicons name="close" size={28} />
+        </TouchableOpacity>
+        <Text style={styles.title}>Insights</Text>
+      </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>NO Insights</Text>
     </View>
+    </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#F0F0F0",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#121417",
+  },
+})
